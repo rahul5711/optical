b6db8e260409001888c87339d8cb9db505a8db5d
rahul gothi <rahulberchha@gmail.com>
2022-09-21 23:35:16 +0530

    update

--- server/controllers/product.controller.js
+++ server/controllers/product.controller.js
@@ -19,6 +19,9 @@ module.exports = {
             if (_.isEmpty(Body)) return res.send({ message: "Invalid Query Data" })
             if (Body.Name.trim() === "") return res.send({ message: "Invalid Query Data" })
 
+            const doesExist = await connection.query(`select * from product where Name = '${Body.Name}' and CompanyID = ${CompanyID} and Status = 1`)
+            if (doesExist.length) return res.send({ message: `Product Already exist from this Name ${Body.Name}` })
+
             const query = await _Query.getQuery("Product", Body, LoggedOnUser, CompanyID,  ShopID)
             const saveData = await connection.query(query)
 
@@ -47,6 +50,8 @@ module.exports = {
             console.log(Body);
             if (_.isEmpty(Body)) return res.send({ message: "Invalid Query Data" })
             if (Body.Name.trim() === "") return res.send({ message: "Invalid Query Data" })
+            const doesExist = await connection.query(`select * from product where Name = '${Body.Name}' and CompanyID = ${CompanyID} and Status = 1`)
+            if (doesExist.length) return res.send({ message: `Product Already exist from this Name ${Body.Name}` })
 
             const query = await _Query.getQuery("Product", Body, LoggedOnUser, CompanyID,  ShopID)
             const saveData = await connection.query(query)
@@ -77,8 +82,11 @@ module.exports = {
             if (_.isEmpty(Body)) return res.send({ message: "Invalid Query Data" })
             if (Body.ID === null) return res.send({ message: "Invalid Query Data" })
             if (Body.TableName === "") return res.send({ message: "Invalid Query Data" })
+            const getProduct = await connection.query(`select * from product where ID = ${Body.ID} and CompanyID = ${CompanyID} and Status = 1`)
 
-            const saveData = await connection.query(`update ${Body.TableName} set Status = 0, UpdatedBy = ${LoggedOnUser.ID}, UpdatedOn = now() where ID = ${Body.ID} and CompanyID = ${LoggedOnUser.ID}`)
+            const doesExist = await connection.query(`select * from productspec where ProductName = '${getProduct[0].Name}' and Status = 1 and CompanyID = ${CompanyID}`)
+            if (doesExist.length) return res.send({ message: `you have to delete spec data first before you can delete product` })
+            const saveData = await connection.query(`update ${Body.TableName} set Status = 0, UpdatedBy = ${LoggedOnUser.ID}, UpdatedOn = now() where ID = ${Body.ID} and CompanyID = ${CompanyID}`)
 
             console.log(connected("Data Delete SuccessFUlly !!!"));
 
@@ -146,6 +154,102 @@ module.exports = {
             return error
 
         }
-    }
+    },
+
+
+    saveSpec: async (req, res, next) => { 
+        try {
+            const response = { data: null, success: true, message: "" }
+            const connection = await getConnection.connection();
+
+            const Body = req.body;
+            const LoggedOnUser = {ID : 0}
+            const CompanyID = 0
+            const ShopID = 0
+            console.log(Body);
+            if (_.isEmpty(Body)) return res.send({ message: "Invalid Query Data" })
+           
+            if(Body.Type === 'DropDown') {
+                Body.SptTableName = Body.ProductName + Math.floor(Math.random() * 999999) + 1 ;
+            } else {
+                Body.SptTableName = '0' 
+            }
+
+            const query = await _Query.getQuery("ProductSpec", Body, LoggedOnUser, CompanyID,  ShopID)
+            const saveData = await connection.query(query)
+
+            console.log(connected("Data Save SuccessFUlly !!!"));
+
+            response.message = "data save sucessfully"
+            response.data = saveData
+            connection.release()
+            return res.send(response)
+
+        } catch (error) {
+            console.log(error);
+            return error
+
+        }
+    },
+
+    deleteSpec: async (req, res, next) => { 
+        try {
+            const response = { data: null, success: true, message: "" }
+            const connection = await getConnection.connection();
+
+            const Body = req.body;
+            const LoggedOnUser = {ID : 0}
+            const CompanyID = 0
+            const ShopID = 0
+            console.log(Body);
+            if (_.isEmpty(Body)) return res.send({ message: "Invalid Query Data" })
+            if (Body.ID === null) return res.send({ message: "Invalid Query Data" })
+            if (Body.TableName === "") return res.send({ message: "Invalid Query Data" })
+
+            const saveData = await connection.query(`update ${Body.TableName} set Status = 0, UpdatedBy = ${LoggedOnUser.ID}, UpdatedOn = now() where ID = ${Body.ID} and CompanyID = ${LoggedOnUser.ID}`)
+
+            console.log(connected("Data Delete SuccessFUlly !!!"));
+
+            response.message = "data delete sucessfully"
+            response.data = saveData
+            connection.release()
+            return res.send(response)
+
+        } catch (error) {
+            console.log(error);
+            return error
+
+        }
+    },
+
+
+    getSpec: async (req, res, next) => { 
+        try {
+            const response = { data: null, success: true, message: "" }
+            const connection = await getConnection.connection();
+
+            const Body = req.body;
+            const LoggedOnUser = {ID : 0}
+            const CompanyID = 0
+            const ShopID = 0
+            if (_.isEmpty(Body)) return res.send({ message: "Invalid Query Data" })
+            if (Body.ProductName.trim() === "") return res.send({ message: "Invalid Query Data" })
+
+            const query = `select * from productspec where ProductName = '${Body.ProductName}' and Status = 1`
+            const saveData = await connection.query(query)
+
+            console.log(connected("Data Fetch SuccessFUlly !!!"));
+
+            response.message = "data fetch sucessfully"
+            response.data = saveData
+            connection.release()
+            return res.send(response)
+
+        } catch (error) {
+            console.log(error);
+            return error
+
+        }
+    },
 
 }
