28acc06e415108dfaa4637c786263742a3ee32cc
rahul gothi <rahulberchha@gmail.com>
2022-10-14 00:08:54 +0530

    update all

--- server/controllers/support.controller.js
+++ server/controllers/support.controller.js
@@ -9,6 +9,7 @@ const connected = chalk.bold.cyan;
 
 module.exports = {
 
+    // support data api
 
     save: async (req, res, next) => {
         try {
@@ -16,8 +17,8 @@ module.exports = {
             const connection = await getConnection.connection();
 
             const Body = req.body;
-            const LoggedOnUser = req.user.ID  ? req.user.ID: 0;
-            const CompanyID = req.user.CompanyID  ? req.user.CompanyID : 0;
+            const LoggedOnUser = req.user.ID ? req.user.ID : 0;
+            const CompanyID = req.user.CompanyID ? req.user.CompanyID : 0;
 
             if (_.isEmpty(Body)) return res.send({ message: "Invalid Query Data" })
             if (_.isEmpty(Body.TableName)) return res.send({ message: "Invalid Query Data" })
@@ -31,7 +32,7 @@ module.exports = {
             console.log(connected("Data Added SuccessFUlly !!!"));
 
             response.message = "data save sucessfully"
-            response.data =  await connection.query(`select * from supportmaster where Status = 1 and CompanyID = '${CompanyID}' and TableName = '${Body.TableName}' order by ID desc`)
+            response.data = await connection.query(`select * from supportmaster where Status = 1 and CompanyID = '${CompanyID}' and TableName = '${Body.TableName}' order by ID desc`)
             connection.release()
             return res.send(response)
         } catch (error) {
@@ -45,14 +46,14 @@ module.exports = {
             const connection = await getConnection.connection();
 
             const Body = req.body;
-            const LoggedOnUser = req.user.ID  ? req.user.ID: 0;
-            const CompanyID = req.user.CompanyID  ? req.user.CompanyID : 0;
+            const LoggedOnUser = req.user.ID ? req.user.ID : 0;
+            const CompanyID = req.user.CompanyID ? req.user.CompanyID : 0;
 
             if (_.isEmpty(Body)) return res.send({ message: "Invalid Query Data" })
             if (_.isEmpty(Body.TableName)) return res.send({ message: "Invalid Query Data" })
 
             response.message = "fetch data sucessfully"
-            response.data =  await connection.query(`select * from supportmaster where Status = 1 and CompanyID = '${CompanyID}' and TableName = '${Body.TableName}' order by ID desc`)
+            response.data = await connection.query(`select * from supportmaster where Status = 1 and CompanyID = '${CompanyID}' and TableName = '${Body.TableName}' order by ID desc`)
             connection.release()
             return res.send(response)
         } catch (error) {
@@ -66,14 +67,14 @@ module.exports = {
             const connection = await getConnection.connection();
 
             const Body = req.body;
-            const LoggedOnUser = req.user.ID  ? req.user.ID: 0;
-            const CompanyID = req.user.CompanyID  ? req.user.CompanyID : 0;
+            const LoggedOnUser = req.user.ID ? req.user.ID : 0;
+            const CompanyID = req.user.CompanyID ? req.user.CompanyID : 0;
 
             if (_.isEmpty(Body)) return res.send({ message: "Invalid Query Data" })
             if (_.isEmpty(Body.TableName)) return res.send({ message: "Invalid Query Data" })
             if (_.isEmpty(Body.Name)) return res.send({ message: "Invalid Query Data" })
 
-            const deleteData =  await connection.query(`update supportmaster set Status = 0, UpdatedBy = '${LoggedOnUser}', UpdatedOn = now() where Name = '${Body.Name}' and CompanyID = '${CompanyID}' and TableName = '${Body.TableName}'`)
+            const deleteData = await connection.query(`update supportmaster set Status = 0, UpdatedBy = '${LoggedOnUser}', UpdatedOn = now() where Name = '${Body.Name}' and CompanyID = '${CompanyID}' and TableName = '${Body.TableName}'`)
 
             console.log(connected("Data Delete SuccessFUlly !!!"));
 
@@ -86,6 +87,81 @@ module.exports = {
             return error
         }
     },
-   
+
+    //  charge master api
+
+    chargesave: async (req, res, next) => {
+        try {
+            const response = { data: null, success: true, message: "" }
+            const connection = await getConnection.connection();
+
+            const Body = req.body;
+            const LoggedOnUser = req.user.ID ? req.user.ID : 0;
+            const CompanyID = req.user.CompanyID ? req.user.CompanyID : 0;
+
+            if (_.isEmpty(Body)) return res.send({ message: "Invalid Query Data" })
+            if (_.isEmpty(Body.Name)) return res.send({ message: "Invalid Query Data" })
+
+            const doesExist = await connection.query(`select * from chargermaster where CompanyID = '${CompanyID}' and Status = 1 and Name = '${Body.Name}'`)
+            if (doesExist.length) return res.send({ message: `Data Already exist from this Name ${Body.Name}` })
+
+            const saveData = await connection.query(`insert into chargermaster (CompanyID, Name, Description, Price,  GSTPercentage, GSTAmount, GSTType, TotalAmount, Status, CreatedBy , CreatedOn ) values (${CompanyID},'${Body.Name}','${Body.Description}', ${Body.Price}, ${Body.GSTPercentage},${Body.GSTAmount},'${Body.GSTType}',${Body.TotalAmount}, 1, ${LoggedOnUser}, now())`)
+
+            console.log(connected("Data Added SuccessFUlly !!!"));
+
+            response.message = "data save sucessfully"
+            response.data = await connection.query(`select * from chargermaster where Status = 1 and CompanyID = '${CompanyID}' order by ID desc`)
+            connection.release()
+            return res.send(response)
+        } catch (error) {
+            console.log(error);
+            return error
+        }
+    },
+
+    chargelist: async (req, res, next) => {
+        try {
+            const response = { data: null, success: true, message: "" }
+            const connection = await getConnection.connection();
+            const LoggedOnUser = req.user.ID ? req.user.ID : 0;
+            const CompanyID = req.user.CompanyID ? req.user.CompanyID : 0;
+
+            response.message = "fetch data sucessfully"
+            response.data = await connection.query(`select * from chargermaster where Status = 1 and CompanyID = '${CompanyID}' order by ID desc`)
+            connection.release()
+            return res.send(response)
+        } catch (error) {
+            console.log(error);
+            return error
+        }
+    },
+
+
+    chargedelete: async (req, res, next) => {
+        try {
+            const response = { data: null, success: true, message: "" }
+            const connection = await getConnection.connection();
+
+            const Body = req.body;
+            const LoggedOnUser = req.user.ID ? req.user.ID : 0;
+            const CompanyID = req.user.CompanyID ? req.user.CompanyID : 0;
+
+            if (_.isEmpty(Body)) return res.send({ message: "Invalid Query Data" })
+            if (_.isEmpty(Body.Name)) return res.send({ message: "Invalid Query Data" })
+
+            const deleteData = await connection.query(`update chargermaster set Status = 0, UpdatedBy = '${LoggedOnUser}', UpdatedOn = now() where Name = '${Body.Name}' and CompanyID = '${CompanyID}'`)
+
+            console.log(connected("Data Delete SuccessFUlly !!!"));
+
+            response.message = "data delete sucessfully"
+            response.data = await connection.query(`select * from chargermaster where Status = 1 and CompanyID = '${CompanyID}' order by ID desc`)
+            connection.release()
+            return res.send(response)
+        } catch (error) {
+            console.log(error);
+            return error
+        }
+    },
+
 
 }
--- server/routes/support.route.js
+++ server/routes/support.route.js
@@ -3,11 +3,16 @@ const router = express.Router()
 const Controller = require('../controllers/support.controller')
 const { verifyAccessTokenAdmin } = require('../helpers/jwt_helper');
 
+// support data 
+
 router.post('/save', verifyAccessTokenAdmin, Controller.save)
 router.post('/list', verifyAccessTokenAdmin, Controller.list)
 router.post('/delete', verifyAccessTokenAdmin, Controller.delete)
 
-
+// charge and service management
+router.post('/chargesave', verifyAccessTokenAdmin, Controller.chargesave)
+router.get('/chargelist', verifyAccessTokenAdmin, Controller.chargelist)
+router.post('/chargedelete', verifyAccessTokenAdmin, Controller.chargedelete)
 
 
 module.exports = router
