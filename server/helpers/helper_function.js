1c4fa940fea9b120819b6005016c93b32ec80ec7
rahul gothi <rahulberchha@gmail.com>
2022-12-20 23:55:44 +0530

    global function for barcode generate

--- server/helpers/helper_function.js
+++ server/helpers/helper_function.js
@@ -17,5 +17,19 @@ module.exports = {
       const visitNo = await connection.query(`select * from ${TableName} where CompanyID = ${CompanyID} and CustomerID = ${CustomerID}`)
 
       return visitNo.length + 1;
+    },
+    generateBarcode : async(CompanyID, BarcodeType) => {
+      const connection = await getConnection.connection();
+      const barcode = await connection.query(`select barcode.${BarcodeType} from barcode where Status = 1 and CompanyID=${CompanyID}`);
+      if (BarcodeType==='SB') {
+        const updateBarcode = await connection.query(`update barcode set ${BarcodeType} = ${Number(barcode[0].SB) + 1}`)
+        return Number(barcode[0].SB)
+      } else if(BarcodeType==='PB') {
+        const updateBarcode = await connection.query(`update barcode set ${BarcodeType} = ${Number(barcode[0].PB) + 1}`)
+        return Number(barcode[0].PB)
+      } else if(BarcodeType==='MB') {
+        const updateBarcode = await connection.query(`update barcode set ${BarcodeType} = ${Number(barcode[0].MB) + 1}`)
+        return Number(barcode[0].MB)
+      }
     }
   }
