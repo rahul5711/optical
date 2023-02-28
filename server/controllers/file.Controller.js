c7d46379adc173819f47c5c989c9ea3df3fb6491
rahul gothi <rahulberchha@gmail.com>
2023-02-28 22:48:47 +0530

    update all customer storage

--- server/controllers/file.Controller.js
+++ server/controllers/file.Controller.js
@@ -65,6 +65,25 @@ module.exports = {
       return next(error)
     }
   },
+  customerupload: async (req, res, next) => {
+    try {
+      const CompanyID = req.user.CompanyID ? req.user.CompanyID : 0
+
+      if (req.file == undefined) {
+        return res.status(400).json({ message: "Please upload file!" });
+      }
+      return res.json({
+        success: true,
+        message: "Uploaded Successfully",
+        file: req.file,
+        fileName: req.file.filename,
+        download: '/uploads/' + year + '/' + month + '/' + CompanyID + '/' + 'customer/' + req.file.filename
+      });
+
+    } catch (error) {
+      return next(error)
+    }
+  },
 
   download: (req, res) => {
     filepath = path.join(__dirname, "/../") + req.params.folder1 + '/' + req.params.folder2 + '/' + req.params.folder3 + '/' + req.params.folder4 
--- server/routes/file.route.js
+++ server/routes/file.route.js
@@ -6,10 +6,12 @@ const FileController = require('../controllers/file.Controller')
 const imageStorageHelper = require('../helpers/diskStorageHelper/image.Storage')
 const companyimageStorageHelper = require('../helpers/diskStorageHelper/companyimage.Storage')
 const purchaseStorageHelper = require('../helpers/diskStorageHelper/purchase.Storage')
+const customerStorageHelper = require('../helpers/diskStorageHelper/customer.Storage')
 
 
 _router.post("/upload", imageStorageHelper.storage.single("file"), FileController.upload);
 _router.post("/companyimage", verifyAccessTokenAdmin, companyimageStorageHelper.storage.single("file"), FileController.companyimageupload);
 _router.post("/purchase", verifyAccessTokenAdmin, purchaseStorageHelper.storage.single("file"), FileController.purchaseupload);
+_router.post("/customer", verifyAccessTokenAdmin, customerStorageHelper.storage.single("file"), FileController.customerupload);
 _router.get("/:folder1/:folder2/:folder3/:folder4/:filename", FileController.download);
 module.exports = _router;
