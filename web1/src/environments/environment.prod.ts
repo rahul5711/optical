0ce0c466ac78c87775e31169d4c3260d39f05055
rahul gothi <rahulberchha@gmail.com>
2023-08-26 19:05:57 +0530

    update

--- web1/src/environments/environment.prod.ts
+++ web1/src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://clearvisionkhairthal.com:50080',
-  appUrl: 'http://clearvisionkhairthal.com/web',
+  apiUrl: 'https://dev.relinksys.com',
+  appUrl: 'https://dev.relinksys.com',
+  // apiUrl: 'http://clearvisionkhairthal.com:50080',
+  // appUrl: 'http://clearvisionkhairthal.com/web',
 };
