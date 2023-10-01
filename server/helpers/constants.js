c1348c233745f698d4476b6c1956866b842fcb55
rahul gothi <rahulberchha@gmail.com>
2023-10-01 22:48:30 +0530

    update

--- server/helpers/constants.js
+++ server/helpers/constants.js
@@ -1,6 +1,6 @@
 module.exports = {
     // appURL: "http://clearvisionkhairthal.com:50080/",
     // appURL: "https://dev.relinksys.com/",
-    appURL: "https://theopticalguru.relinksys.com",
-    // appURL: "http://localhost:3000/",
+    // appURL: "https://theopticalguru.relinksys.com",
+    appURL: "http://localhost:3000/",
 }
