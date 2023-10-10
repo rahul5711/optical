6de0b11409b725994d581a17648d22ed0f6e53b8
vinaytayade <vinay@930>
2023-10-10 20:28:29 +0530

    UPADTE

--- web1/src/app/admin/dashborad.component.ts
+++ web1/src/app/admin/dashborad.component.ts
@@ -7,7 +7,7 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
   styleUrls: ['./dashborad.component.css']
 })
 export class DashboradComponent implements OnInit {
-
+  user:any =JSON.parse(localStorage.getItem('user') || '') ;
   constructor( 
     private modalService: NgbModal,
   ) { }
@@ -15,62 +15,86 @@ export class DashboradComponent implements OnInit {
   cards: any = []
  
   ngOnInit(): void {
-
-   let dashcard = [
-    // {
-    //   "icon" : "assets/images/customer.png",
-    //   "title": "Customer",
-    //   "routersLinks" : "/sale/billing/0/0"
-    // },
-    {
-      "icon" : "assets/images/billing.png",
-      "title": "Customer",
-      "routersLinks" : "/sale/billing/0/0"
-    },
-    {
-      "icon" : "assets/images/search-list.png",
-      "title": "Bill Search",
-      "routersLinks" : "/sale/billinglist/0"
-    },
-    {
-      "icon" : "assets/images/purchase.png",
-      "title": "Purchase",
-      "routersLinks" : "/inventory/purchase/0"
-    },
-    {
-      "icon" : "assets/images/expense.png",
-      "title": "Expense",
-      "routersLinks" : "/companyPayment/expense"
-    },
-    // {
-    //   "icon" : "assets/images/supp.png",
-    //   "title": "Supplier",
-    //   "routersLinks" : "/inventory/supplier"
-    // },
+    let dashcard
+   if(this.user.UserGroup === 'CompanyAdmin'){
+      dashcard = [
+      {
+        "icon" : "assets/images/billing.png",
+        "title": "Customer ",
+        "routersLinks" : "/sale/billing/0/0"
+      },
+      {
+        "icon" : "assets/images/search-list.png",
+        "title": "Bill Search",
+        "routersLinks" : "/sale/billinglist/0"
+      },
+      {
+        "icon" : "assets/images/purchase.png",
+        "title": "Purchase",
+        "routersLinks" : "/inventory/purchaseList/0"
+      },
+      {
+        "icon" : "assets/images/expense.png",
+        "title": "Expense",
+        "routersLinks" : "/companyPayment/expense"
+      },
+      {
+        "icon" : "assets/images/transfer.png",
+        "title": "Product Transfer",
+        "routersLinks" : "/inventory/product-transfer"
+      },
+      {
+        "icon" : "assets/images/sms-removebg-preview.png",
+        "title": "Bulk SMS",
+        "routersLinks" : "/admin/smsSetting"
+      },
+      {
+        "icon" : "assets/images/reminder.png",
+        "title": "Reminder",
+        "routersLinks" : "/product/productManage"
+      },
+      {
+        "icon" : "assets/images/sms-removebg-preview.png",
+        "title": "Summary",
+        "routersLinks" : "/admin/dashboardSummary"
+      },
   
-    {
-      "icon" : "assets/images/transfer.png",
-      "title": "Product Transfer",
-      "routersLinks" : "/inventory/product-transfer"
-    },
-    {
-      "icon" : "assets/images/sms-removebg-preview.png",
-      "title": "Bulk SMS",
-      "routersLinks" : "/admin/smsSetting"
-    },
-  
-    {
-      "icon" : "assets/images/reminder.png",
-      "title": "Reminder",
-      "routersLinks" : "/product/productManage"
-    },
-    {
-      "icon" : "assets/images/sms-removebg-preview.png",
-      "title": "Summary",
-      "routersLinks" : "/admin/dashboardSummary"
-    },
+     ]
+   }else{
+    dashcard = [
+      {
+        "icon" : "assets/images/billing.png",
+        "title": "Customer ",
+        "routersLinks" : "/sale/billing/0/0"
+      },
+      {
+        "icon" : "assets/images/search-list.png",
+        "title": "Bill Search",
+        "routersLinks" : "/sale/billinglist/0"
+      },
+      {
+        "icon" : "assets/images/expense.png",
+        "title": "Expense",
+        "routersLinks" : "/companyPayment/expense"
+      },
+      {
+        "icon" : "assets/images/transfer.png",
+        "title": "Product Transfer",
+        "routersLinks" : "/inventory/product-transfer"
+      },
+      {
+        "icon" : "assets/images/sms-removebg-preview.png",
+        "title": "Bulk SMS",
+        "routersLinks" : "/admin/smsSetting"
+      },
+      {
+        "icon" : "assets/images/reminder.png",
+        "title": "Reminder",
+        "routersLinks" : "/product/productManage"
+      }, 
+     ]
+   }
 
-   ]
 
    this.cards = dashcard
   }
