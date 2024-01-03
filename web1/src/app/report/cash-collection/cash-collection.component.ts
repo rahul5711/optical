3c883d29f41bc381f10a8f932ab03577aabfa161
vinaytayade <vinay@930>
2024-01-03 19:05:27 +0530

    update

--- web1/src/app/report/cash-collection/cash-collection.component.ts
+++ web1/src/app/report/cash-collection/cash-collection.component.ts
@@ -7,6 +7,8 @@ import { NgxSpinnerService } from 'ngx-spinner';
 import * as XLSX from 'xlsx';
 import { SupportService } from 'src/app/service/support.service';
 import { BillService } from 'src/app/service/bill.service';
+import { environment } from 'src/environments/environment';
+import { NgxPrintDirective } from 'ngx-print';
 
 @Component({
   selector: 'app-cash-collection',
@@ -15,8 +17,9 @@ import { BillService } from 'src/app/service/bill.service';
 })
 
 export class CashCollectionComponent implements OnInit {
-  shop:any =JSON.parse(localStorage.getItem('shop') || '') ;
-  user:any =JSON.parse(localStorage.getItem('user') || '') ;
+  env = environment;
+  shop: any = JSON.parse(localStorage.getItem('shop') || '');
+  user: any = JSON.parse(localStorage.getItem('user') || '');
   permission = JSON.parse(localStorage.getItem('permission') || '[]');
   companySetting = JSON.parse(localStorage.getItem('companysetting') || '');
   selectedShop = JSON.parse(localStorage.getItem('selectedShop') || '');
@@ -30,18 +33,18 @@ export class CashCollectionComponent implements OnInit {
 
   ) { }
 
-  shopList:any;
-  selectsShop :any;
-  selectsShopimg :any;
-  PaymentModesList:any =[];
-  dataList:any =[];
+  shopList: any;
+  selectsShop: any;
+  selectsShopimg: any;
+  PaymentModesList: any = [];
+  dataList: any = [];
 
-  data: any =  { 
-    FilterTypes:'CreatedOn', FromDate: moment().format('YYYY-MM-DD'), ToDate: moment().format('YYYY-MM-DD'), ShopID: 0,PaymentStatus:0,
-    PaymentMode:0
+  data: any = {
+    FilterTypes: 'CreatedOn', FromDate: moment().format('YYYY-MM-DD'), ToDate: moment().format('YYYY-MM-DD'), ShopID: 0, PaymentStatus: 0,
+    PaymentMode: 0
   };
 
-  paymentMode:any = []
+  paymentMode: any = []
   sumOfPaymentMode = 0
   AmountReturnByCredit = 0
   AmountReturnByDebit = 0
@@ -64,14 +67,16 @@ export class CashCollectionComponent implements OnInit {
       }
     });
 
-    if(this.user.UserGroup === 'Employee'){
-      this.shopList  = this.shop;
+    if (this.user.UserGroup === 'Employee') {
+      this.shopList = this.shop;
       this.data.ShopID = this.shopList[0].ShopID
-    }else{
+    } else {
       this.dropdownShoplist();
     }
     this.getPaymentModesList()
   }
+
+
 
   getPaymentModesList() {
     const subs: Subscription = this.supps.getList('PaymentModeType').subscribe({
@@ -87,21 +92,17 @@ export class CashCollectionComponent implements OnInit {
     });
   }
 
-  dropdownShoplist(){
+  dropdownShoplist() {
     const subs: Subscription = this.ss.dropdownShoplist('').subscribe({
       next: (res: any) => {
-        this.shopList  = res.data
-        let shop = res.data
-        this.selectsShop = shop.filter((s:any) => s.ID === Number(this.selectedShop[0]));
-        this.selectsShop =  '/ ' + this.selectsShop[0].Name + ' (' + this.selectsShop[0].AreaName + ')'
-        this.selectsShopimg =  '/ ' + this.selectsShop[0].LogoURL
+        this.shopList = res.data
       },
       error: (err: any) => console.log(err.message),
       complete: () => subs.unsubscribe(),
     });
   }
 
-  getCashReport(){
+  getCashReport() {
     this.sp.show()
     let Parem = '';
     this.paymentMode = [];
@@ -110,24 +111,26 @@ export class CashCollectionComponent implements OnInit {
     this.AmountReturnByDebit = 0;
     this.totalAmount = 0;
 
-    if (this.data.FromDate !== '' && this.data.FromDate !== null){
-      let FromDate =  moment(this.data.FromDate).format('YYYY-MM-DD')
-      Parem = Parem + ' and DATE_FORMAT(paymentmaster.PaymentDate, "%Y-%m-%d") between ' +  `'${FromDate}'`;}
+    if (this.data.FromDate !== '' && this.data.FromDate !== null) {
+      let FromDate = moment(this.data.FromDate).format('YYYY-MM-DD')
+      Parem = Parem + ' and DATE_FORMAT(paymentmaster.PaymentDate, "%Y-%m-%d") between ' + `'${FromDate}'`;
+    }
 
-    if (this.data.ToDate !== '' && this.data.ToDate !== null){
-      let ToDate =  moment(this.data.ToDate).format('YYYY-MM-DD')
-      Parem = Parem + ' and ' +  `'${ToDate}'`;}
+    if (this.data.ToDate !== '' && this.data.ToDate !== null) {
+      let ToDate = moment(this.data.ToDate).format('YYYY-MM-DD')
+      Parem = Parem + ' and ' + `'${ToDate}'`;
+    }
 
-     let dtm = {
-      Date : Parem ,
-      ShopID : this.data.ShopID ? this.data.ShopID : 0,
-      PaymentMode : this.data.PaymentMode ? this.data.PaymentMode   : 0,
-      PaymentStatus : this.data.PaymentStatus ? this.data.PaymentStatus : 0,
-     }
+    let dtm = {
+      Date: Parem,
+      ShopID: this.data.ShopID ? this.data.ShopID : 0,
+      PaymentMode: this.data.PaymentMode ? this.data.PaymentMode : 0,
+      PaymentStatus: this.data.PaymentStatus ? this.data.PaymentStatus : 0,
+    }
 
-    const subs: Subscription =  this.billService.cashcollectionreport(dtm).subscribe({
+    const subs: Subscription = this.billService.cashcollectionreport(dtm).subscribe({
       next: (res: any) => {
-        if(res.success){
+        if (res.success) {
           this.as.successToast(res.message)
           this.dataList = res.data
           this.paymentMode = res.paymentMode
@@ -136,7 +139,7 @@ export class CashCollectionComponent implements OnInit {
           this.AmountReturnByDebit = res.AmountReturnByDebit
           this.totalAmount = res.totalAmount
           this.totalCalculation(this.dataList);
-        }else{
+        } else {
           this.as.errorToast(res.message)
         }
         this.sp.hide()
@@ -150,18 +153,18 @@ export class CashCollectionComponent implements OnInit {
   totalCalculation(data: any) {
     this.newPayment = 0;
     this.oldPayment = 0;
-  
+
     for (var i = 0; i < data.length; i++) {
       const billDate = moment(data[i].BillDate).format('YYYY-MM-DD');
       const fromDate = moment(this.data.FromDate).format('YYYY-MM-DD');
       const toDate = moment(this.data.ToDate).format('YYYY-MM-DD');
-  
+
       if (billDate !== 'Invalid date' && data[i].PaymentStatus !== null && new Date(billDate) >= new Date(fromDate) && new Date(billDate) <= new Date(toDate)) {
         this.newPayment += Number(data[i].Amount);
       } else {
         this.oldPayment += Number(data[i].Amount);
       }
-      
+
     }
   }
 
@@ -172,16 +175,124 @@ export class CashCollectionComponent implements OnInit {
     XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
     XLSX.writeFile(wb, 'Cash_Collection_Report.xlsx');
   }
-  
-  dateFormat(date:any){
+
+  dateFormat(date: any) {
     return moment(date).format(`${this.companySetting.DateFormat}`);
   }
 
-  FromReset(){
-    this.data = { 
-      FilterTypes:'CreatedOn', FromDate: moment().format('YYYY-MM-DD'), ToDate: moment().format('YYYY-MM-DD'), ShopID: 0,PaymentStatus:0,
-      PaymentMode:0
+  FromReset() {
+    this.data = {
+      FilterTypes: 'CreatedOn', FromDate: moment().format('YYYY-MM-DD'), ToDate: moment().format('YYYY-MM-DD'), ShopID: 0, PaymentStatus: 0,
+      PaymentMode: 0
     };
     this.dataList = [];
+  }
+  ngAfterViewInit() {
+    setTimeout(() => {
+      this.print();
+    }, 1000); // Adjust the delay as needed
+  }
+  print() {
+    let shop = this.shopList
+    this.selectsShop = shop.filter((s: any) => s.ID === Number(this.selectedShop[0]));
+
+
+    let printContent: any = document.getElementById('print-content');
+    let printWindow: any = window.open('pp', '_blank');
+    printWindow.document.write(`
+      <html>
+        <head>
+        <title>Cash Collection Report</title>
+          <style>
+            @media print {
+
+              body {
+                margin:0;
+                padding:0;
+                zoom:100%;
+                width:100%;
+                font-family: 'Your Font Family', sans-serif;
+              }
+              .header-body{
+                width:100%;
+                height:220px;
+
+              }
+              .main-body{
+                width:100%;
+              }
+              .header-body .print-title {
+                width:45%;
+                text-align: left;
+                margin-bottom: 20px;
+                float:right;
+              }
+              .header-body .print-logo {
+                width:50%;
+                text-align: center;
+                margin-bottom: 0px;
+                float:left;
+              }
+              .print-logo img{
+                width: 100%;
+                height: 200px;
+                object-fit: contain;
+              }
+              thead{
+                background-color: #dcdcdc;
+                height:50px;
+              }
+              thead tr{
+                height:30px;
+              }
+              th{
+                padding:0px;
+                margin:0px;
+
+              }
+              table  {
+                padding:0px;
+                margin:0px;
+              }
+              td  {
+                padding:0px;
+                margin:0px;
+              }
+              tr:nth-child(even) {
+                background-color: #f2f2f2;
+            }
+            th.hide-on-print,tr.hide-on-print,
+            td.hide-on-print {
+              display: none;
+            }
+            tfoot.hide-on-print {
+              display: block;
+            }
+            }
+          </style>
+        </head>
+        <body>
+        <div class="header-body">
+          <div class="print-logo ">
+            <img src="${this.env.apiUrl + this.selectsShop[0].LogoURL}" alt="Logo" >
+          </div>
+          <div class="print-title">
+            <h1>${this.selectsShop[0].Name + ' (' + this.selectsShop[0].AreaName + ')'}</h1>
+            <h2>${this.selectsShop[0].Address}</h2>
+          </div>
+        </div>
+        <div class="main-body">
+          ${printContent.innerHTML}
+        </div>
+        </body>
+      </html>
+    `);
+
+    printWindow.document.querySelectorAll('.hide-on-print').forEach((element: any) => {
+      element.classList.add('hide-on-print');
+    });
+
+    printWindow.document.close();
+    printWindow.print();
   }
 }
