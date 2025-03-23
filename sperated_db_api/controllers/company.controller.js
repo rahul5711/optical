b260cccdf768a55ab49f3b977a90c981fbfdd9c0
RahulGothi8340 <100769311+Rahulvoso@users.noreply.github.com>
2025-03-23 15:07:07 +0530

    update

--- sperated_db_api/controllers/company.controller.js
+++ sperated_db_api/controllers/company.controller.js
@@ -1324,7 +1324,7 @@ module.exports = {
         let connection;
         try {
 
-            const response = { data: null, success: true, message: "", sumOfCaAmount: 0 }
+            const response = { data: null, success: true, message: "", sumOfCaAmount: 0, sumNoOfShops: 0 }
             const { Parem } = req.body;
             if (Parem === "" || Parem === undefined || Parem === null) {
                 return res.send({ message: "Invalid Query Data" })
@@ -1336,6 +1336,7 @@ module.exports = {
 
             if (data.length) {
                 for (let item of data) {
+                    response.sumNoOfShops += Number(item.NoOfShops)
                     if (item.CAmount !== "") {
                         response.sumOfCaAmount += Number(item.CAmount);
                     }
