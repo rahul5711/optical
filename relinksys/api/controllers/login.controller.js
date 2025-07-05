7fd558e575b35280e2da2dc3a3245ec62c976c86
RahulGothi8340 <100769311+Rahulvoso@users.noreply.github.com>
2025-07-05 13:22:40 +0530

    update

--- relinksys/api/controllers/login.controller.js
+++ relinksys/api/controllers/login.controller.js
@@ -81,7 +81,7 @@ module.exports = {
                         `Insert into loginhistory (CompanyID, UserName, UserID, LoginTime, IpAddress, Comment) values (${User[0].CompanyID}, '${User[0].Name}', ${User[0].ID}, now(), '${ip}', '${comment}')`
 
                     );
-                    const [shop] = await connection.query(`select * from shop where CompanyID = '${User[0].CompanyID}'`)
+                    const [shop] = await connection.query(`select * from shop where Status = 1 and CompanyID = '${User[0].CompanyID}'`)
                     return res.send({ message: "User Login sucessfully", data: User[0], Company: company[0], CompanySetting: setting[0], shop: shop, success: true, accessToken: accessToken, refreshToken: refreshToken, loginCode: loginCode })
                 } else {
 
