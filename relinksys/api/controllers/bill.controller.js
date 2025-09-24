756bfbf0b435476310db7cba98be5a453bd336a3
RahulGothi8340 <100769311+Rahulvoso@users.noreply.github.com>
2025-09-24 23:18:55 +0530

    update

--- relinksys/api/controllers/bill.controller.js
+++ relinksys/api/controllers/bill.controller.js
@@ -1086,7 +1086,7 @@ module.exports = {
             if (billMaseterData.Doctor === null || billMaseterData.Doctor === "null" || billMaseterData.Doctor === undefined || billMaseterData.Doctor === "None") {
                 billMaseterData.Doctor = 0
             }
-            if (!billDetailData.length && !service.length) return res.send({ message: "Invalid Query Data" })
+          //  if (!billDetailData.length && !service.length) return res.send({ message: "Invalid Query Data" })
             if (billMaseterData.ID === null || billMaseterData.ID === undefined || billMaseterData.ID == 0 || billMaseterData.ID === "") return res.send({ message: "Invalid Query Data" })
             if (billMaseterData.ShopID === null || billMaseterData.ShopID === undefined || billMaseterData.ShopID == 0 || billMaseterData.ShopID === "") return res.send({ message: "Invalid Query Data" })
             if (billMaseterData.InvoiceNo === null || billMaseterData.InvoiceNo === undefined || billMaseterData.InvoiceNo == 0 || billMaseterData.InvoiceNo === "") return res.send({ message: "Invalid Query InvoiceNo Data" })
