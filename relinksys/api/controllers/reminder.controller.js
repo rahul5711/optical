97d05374aacfd9f62b97cc2b924b4c80a9e4b8a6
Rahulvoso <100769311+Rahulvoso@users.noreply.github.com>
2025-09-25 17:34:14 +0530

    update

--- relinksys/api/controllers/reminder.controller.js
+++ relinksys/api/controllers/reminder.controller.js
@@ -2571,7 +2571,7 @@ async function sendWhatsAppTextMessageNew({ CustomerName, Mobile, ShopName, Shop
         return { success: true, data: response.data };
     } catch (error) {
         console.error('Error sending WhatsApp message:', error.response?.data || error.message);
-        return { success: false, error: error.response?.data || error.message };
+        return { success: false, message: error.response?.data || error.message };
     }
 }
 
