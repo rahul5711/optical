521f254841b1fd228a8b3fc0f294d97344b18e7a
RahulGothi8340 <100769311+Rahulvoso@users.noreply.github.com>
2025-10-11 16:19:44 +0530

    update

--- relinksys/api/app.js
+++ relinksys/api/app.js
@@ -269,6 +269,7 @@ setInterval(heartBeat, 10000);
 const PORT = process.env.PORT || 3000
 
 http.listen(PORT, () => {
+  console.log('Current Node.js version:', process.version);
   loggerss.loggers.info(`server running on port : ${PORT}`)
   connected(`server running on port ${PORT}`)
 });
