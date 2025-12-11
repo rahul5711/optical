7e4fc808e60cf19bf54b062f26443d5a49d8abcb
vinaytayade <vinay@930>
2025-12-11 23:16:28 +0530

    upadte

--- relinksys/web/src/app/product/add-manage/add-manage.component.ts
+++ relinksys/web/src/app/product/add-manage/add-manage.component.ts
@@ -55,6 +55,7 @@ export class AddManageComponent implements OnInit {
     {Name: 'Lens Type',value:'LensType'},
     {Name: 'Axis',value:'Axis'},
     {Name: 'Addition',value:'Addition'},
+    {Name: 'InsuranceCompanyName',value:'InsuranceCompanyName'},
   
   ]
 
