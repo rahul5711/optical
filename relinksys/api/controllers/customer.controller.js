50852681722d2cb96c9bdb37971a6656ffaa044e
vinaytayade <vinay@930>
2026-01-22 20:12:32 +0530

    update

--- relinksys/api/controllers/customer.controller.js
+++ relinksys/api/controllers/customer.controller.js
@@ -1121,6 +1121,13 @@ module.exports = {
                     formatName = "customerPowerPDF.ejs"
                 }
             }
+            if (CompanyID === 341) {
+                if (printdata.mode === 'spectacle') {
+                    formatName = "optivision.ejs";
+                } else {
+                    formatName = "customerPowerPDF.ejs"
+                }
+            }
 
             var file = printdata.mode + "-" + 'Power' + "_" + CompanyID + "-" + customer.ID + ".pdf";
             fileName = "uploads/" + file;
